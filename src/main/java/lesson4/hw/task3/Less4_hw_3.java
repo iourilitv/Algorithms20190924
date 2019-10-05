2b124d15fc330814cad005db7c48afbe5b784811
iourilitv <iourilitv1965@gmail.com>
2019-10-05 16:04:20 +0300

    Made a draft of the task3 of lesson 4 hw.

---
+++ src/main/java/lesson4/hw/task3/Less4_hw_3.java
@@ -0,0 +1,16 @@
+package lesson4.hw.task3;
+
+/**
+ * GBJava3
+ * Algorithms and data structures in Java. 24.09.2019 Webinar. Teacher: Fanzil' Kusyapkulov
+ * Урок 4. Связанные списки.
+ * Учимся создавать и использовать списки.
+ * Домашняя работа.
+ * 3. Реализовать класс Очередь.
+ * 4* Реализовать ListIterator ( Необязательно )
+ */
+public class Less4_hw_3 {
+    public static void main(String[] args) {
+        new TestL4_3().run();
+    }
+}
