2a77c221f57d5a58f4672f89710c2ffffdee3025
iourilitv <iourilitv1965@gmail.com>
2019-10-05 16:03:59 +0300

    Made a draft of the task2 of lesson 4 hw.

---
+++ src/main/java/lesson4/hw/task2/Less4_hw_2.java
@@ -0,0 +1,15 @@
+package lesson4.hw.task2;
+
+/**
+ * GBJava3
+ * Algorithms and data structures in Java. 24.09.2019 Webinar. Teacher: Fanzil' Kusyapkulov
+ * Урок 4. Связанные списки.
+ * Учимся создавать и использовать списки.
+ * Домашняя работа.
+ * 2. В методе main класса Main проверить все методы итератора.
+ */
+public class Less4_hw_2 {
+    public static void main(String[] args) {
+        new TestL4_2().run();
+    }
+}
