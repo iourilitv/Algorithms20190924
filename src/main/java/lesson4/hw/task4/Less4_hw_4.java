6bf1e3c37baacc93d0db44e9b0853de62a7650ba
iourilitv <iourilitv1965@gmail.com>
2019-10-06 11:45:03 +0300

    Made a draft of the task4 of lesson 4 hw.

---
+++ src/main/java/lesson4/hw/task4/Less4_hw_4.java
@@ -0,0 +1,15 @@
+package lesson4.hw.task4;
+
+/**
+ * GBJava3
+ * Algorithms and data structures in Java. 24.09.2019 Webinar. Teacher: Fanzil' Kusyapkulov
+ * Урок 4. Связанные списки.
+ * Учимся создавать и использовать списки.
+ * Домашняя работа.
+ * 4* Реализовать ListIterator ( Необязательно )
+ */
+public class Less4_hw_4 {
+    public static void main(String[] args) {
+        new TestL4_4().run();
+    }
+}
---
+++ src/main/java/lesson4/hw/task4/TestL4_4.java
@@ -0,0 +1,16 @@
+package lesson4.hw.task4;
+
+import java.util.Random;
+
+class TestL4_4 {
+    private Random random = new Random();
+    //MyLinkedQueue<String> queue = new MyLinkedQueue<>();
+
+    void run(){
+
+
+    }
+
+
+
+}
