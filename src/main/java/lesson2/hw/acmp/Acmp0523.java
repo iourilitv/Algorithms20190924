d5a45feb564433d0c3398b6f1f9c50f0553d4cc6
iourilitv <iourilitv1965@gmail.com>
2019-10-08 15:53:28 +0300

    Updated parametrized test and the class of the Task0523.

--- src/main/java/lesson2/hw/acmp/Acmp0523.java
+++ src/main/java/lesson2/hw/acmp/Acmp0523.java
@@ -160,7 +160,7 @@ class Task0523 {
      * @param totalPagesSum - общее количество страниц в романе
      * @return минимально возможный объем(в страницах) самого «толстого» тома.
      */
-    private int findMinAcceptableOfMaxTomeSize(int maxPagesNumber, int totalPagesSum){
+    /*private int findMinAcceptableOfMaxTomeSize(int maxPagesNumber, int totalPagesSum){
         //если том 1
         if(tomesNumber == 1){
             //то возвращаем общее количество страниц в романе
@@ -218,6 +218,87 @@ class Task0523 {
         }
         //если нет - -1
         return -1;
+    }*/
+    private int findMinAcceptableOfMaxTomeSize(int maxPagesNumber, int totalPagesSum){
+        //если том 1
+        if(tomesNumber == 1){
+            //то возвращаем общее количество страниц в романе
+            return totalPagesSum;
+        }
+        //если количество томов равны количеству глав или крайние элементы больше или равны суммы оставшихся
+        if(tomesNumber == chaptersArray.length ||
+                chaptersArray[0] >= totalPagesSum - chaptersArray[0] ||
+                chaptersArray[chaptersArray.length - 1] >= totalPagesSum - chaptersArray[chaptersArray.length - 1]
+            ){
+            //то возвращаем максимальное количество страниц в одной главе
+            return maxPagesNumber;
+        }
+
+        //Запоминаем минимально возможное количество страниц в томе
+        //TODO Deleted
+        //int minSize = 0;
+        //TODO Added
+        //Нижняя граница не может быть меньше максимального числа страниц в главе
+        int minSize = maxPagesNumber - 1;
+
+        //Запоминаем максимально возможное количество страниц в томе
+        int maxSize = totalPagesSum + 1;
+        //Объявляем переменную для средней значения
+        int middleSize;
+
+        //TODO Deleted
+        //Объявляем переменную для искомого значения
+        //int minimumSize = maxSize;
+
+        //***Бинарный поиск***
+        //крутим цикл пока не сблизим края поиска до одного элемента
+        while(maxSize - minSize != 1){
+            //устанавливаем на середине диапазона
+            middleSize = minSize + (maxSize - minSize) / 2;
+            //проверяем не больше ли проверяемое значение максимального количества в одной главе и
+            // выполняется ли условие с размером middleSize
+            boolean result = isCondition(middleSize, totalPagesSum);
+            //проверяем было ли совпадение
+            if(flag){
+                //если да есть хотя бы одно совпадение
+                //TODO Deleted
+                //if(middleSize <= maxPagesNumber || result){
+                //TODO Added
+                if(result){
+
+                    //minSize всегда должна указывать на ситуацию, когда условие выполняется
+                    minSize = middleSize;
+
+                    //TODO Deleted
+                    /*//запоминаем последнее совпавшее значение
+                    if(minimumSize > middleSize){
+                        minimumSize = middleSize;
+                    }*/
+
+                } else {
+                    //maxSize всегда должна указывать на ситуацию, когда условие не выполняется
+                    maxSize = middleSize;
+                }
+            } else{
+                //если нет совпадений
+                minSize++;
+            }
+        }
+
+        //TODO Deleted
+        /*//если была найдена хотя бы одна комбинация
+        if(maxSize >= 0){
+            //возвращаем значение нижней границы диапазона поиска
+            return minimumSize;
+        }*/
+        //TODO Added
+        //если была найдена хотя бы одна комбинация
+        if(minSize > 0){
+            //возвращаем максимальное расстояние, на котором можно расставить коров в стойла
+            return minSize;
+        }
+        //если нет - -1
+        return -1;
     }
 
     /**
@@ -239,26 +320,52 @@ class Task0523 {
         //листаем массив - ищем количество томов, к которых можно разместить все главы
         // при проверяемом размере самого "толстого" тома
         for (int i = 0; i < chaptersArray.length; i++) {
-            //если элемент больше проверяемого значения сбрасываем флаг и выходим
+
+            //TODO Deleted
+            /*//если элемент больше проверяемого значения сбрасываем флаг и выходим
             if(chaptersArray[i] > verifiedSize){
                 flag = false;
                 return false;
-            }
+            }*/
 
             //прибавляем к текущей сумме значение текущего элемента
             currentPagesSum += chaptersArray[i];
 
-            //если есть хотя бы одно совпадение, взводим флаг
+            //TODO Deleted
+            /*//если есть хотя бы одно совпадение, взводим флаг
             if(currentPagesSum == verifiedSize){
                 flag = true;
             }
-
             if(currentPagesSum > verifiedSize){
                 //если текущая сумма больше проверяемого значения тома,
                 //и берем следующий том
                 currentNumber++;
                 //обнуляем текущую сумму
                 currentPagesSum = chaptersArray[i];
+            }*/
+            //TODO Added
+            if(currentPagesSum >= verifiedSize){
+                //если текущая сумма совпадает с проверяемым, взводим флаг
+                if(currentPagesSum == verifiedSize){
+                    //если текущая сумма равна проверяемому значению, взводим флаг и сбрасываем сумму
+                    flag = true;
+                    currentPagesSum = 0;
+                } else {
+                    //если текущая сумма больше проверяемого, то проверяем текущий элемент
+                    if(chaptersArray[i] == verifiedSize){
+                        //если текущий элемент равен проверяемому значению, взводим флаг и сбрасываем сумму
+                        flag = true;
+                        currentPagesSum = 0;
+                    }else{
+                        //если сопадения нет(текущая сумма просто больше проверяемого значения)
+                        //в текущую сумму записываем значение текущего элемента
+                        currentPagesSum = chaptersArray[i];
+                    }
+                }
+                //если текущая элемент не последний инкрементируем счетчик томов
+                if(i != chaptersArray.length - 1){
+                    currentNumber++;
+                }
             }
         }
         //если текущее значение томов больше их заданного количества, возвращаем true
--- src/test/java/lesson2/hw/acmp/Task0523Test.java
+++ src/test/java/lesson2/hw/acmp/Task0523Test.java
@@ -14,31 +14,36 @@ public class Task0523Test {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-//                /*01*/{new Integer[]{1}, 1, 1},
-//                /*02*/{new Integer[]{1, 1}, 1, 2},
-//                /*03*/{new Integer[]{1, 1}, 2, 1},
-//                /*04*/{new Integer[]{1, 2}, 2, 2},
-//                /*05*/{new Integer[]{2, 1}, 2, 2},
-//                /*06*/{new Integer[]{2, 2}, 2, 2},
-//                /*07*/{new Integer[]{1, 1, 1}, 1, 3},
-//                /*08*/{new Integer[]{1, 1, 1}, 2, 2},
-//                /*09*/{new Integer[]{1, 1, 1}, 3, 1},
-//                /*10*/{new Integer[]{1, 1, 2}, 2, 2},
-//                /*11*/{new Integer[]{1, 2, 1}, 2, 3},
-//                /*12*/{new Integer[]{2, 1, 1}, 2, 2},
-//                /*13*/{new Integer[]{2, 1, 2}, 2, 3},
-//                /*14*/{new Integer[]{2, 2, 1}, 2, 3},
-//                /*15*/{new Integer[]{2, 2, 2}, 2, 4},
-                /*16*/{new Integer[]{1, 1, 2, 5}, 1, 4},/*9*/
-                /*17*/{new Integer[]{1, 1, 2, 5}, 2, 4},/*5*/
-                /*18*/{new Integer[]{1, 1, 2, 5}, 3, 4},/*5*/
-//                /*19*/{new Integer[]{1, 1, 2, 5}, 4, 4},
-//                /*20*/{new Integer[]{1, 2, 1, 1}, 3, 2},
-//                /*21*/{new Integer[]{2, 1, 1, 2}, 2, 3},
-//                /*22*/{new Integer[]{1, 2, 3, 4}, 4, 4},
-//                /*23*/{new Integer[]{9, 5, 2, 4, 10}, 3, 11},
-
-
+//                /*0*/{new Integer[]{1}, 1, 1},
+//                /*1*/{new Integer[]{1, 1}, 1, 2},
+//                /*2*/{new Integer[]{1, 1}, 2, 1},
+//                /*3*/{new Integer[]{1, 2}, 2, 2},
+//                /*4*/{new Integer[]{2, 1}, 2, 2},
+//                /*5*/{new Integer[]{2, 2}, 2, 2},
+//                /*6*/{new Integer[]{1, 1, 1}, 1, 3},
+//                /*7*/{new Integer[]{1, 1, 1}, 2, 2},
+//                /*8*/{new Integer[]{1, 1, 1}, 3, 1},
+//                /*9*/{new Integer[]{1, 1, 2}, 2, 2},
+//                /*10*/{new Integer[]{1, 2, 1}, 2, 3},
+//                /*11*/{new Integer[]{2, 1, 1}, 2, 2},
+//                /*12*/{new Integer[]{2, 1, 2}, 2, 3},
+//                /*13*/{new Integer[]{2, 2, 1}, 2, 3},
+//                /*14*/{new Integer[]{2, 2, 2}, 2, 4},
+//                /*15*/{new Integer[]{1, 1, 2, 5}, 1, 9},
+//                /*16*/{new Integer[]{1, 1, 2, 5}, 2, 5},
+//                /*17*/{new Integer[]{1, 1, 2, 5}, 3, 5},
+//                /*18*/{new Integer[]{1, 1, 2, 5}, 4, 5},
+                /*19*/{new Integer[]{2, 1, 1, 2}, 2, 3},/*5*/
+                /*20*/{new Integer[]{1, 2, 1, 1}, 3, 2},/*1*/
+                /*21*/{new Integer[]{1, 2, 3, 4}, 4, 4},
+                /*22*/{new Integer[]{9, 5, 2, 4, 10}, 2, 16},/*29*/
+                /*23*/{new Integer[]{9, 5, 2, 4, 10}, 3, 11},/*13*/
+                /*24*/{new Integer[]{9, 5, 2, 4, 10}, 4, 10},/*9*/
+                /*25*/{new Integer[]{9, 5, 2, 4, 10}, 5, 10},
+                /*26*/{new Integer[]{9, 5, 10, 4, 1}, 2, 15},/*28*/
+                /*27*/{new Integer[]{9, 5, 10, 4, 1}, 3, 14},/*23*/
+                /*28*/{new Integer[]{9, 5, 10, 4, 1}, 4, 10},/*23*/
+                /*29*/{new Integer[]{9, 5, 10, 4, 1}, 5, 10},
         });
     }
     private Integer[] a;
