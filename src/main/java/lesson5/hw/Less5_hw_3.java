af0426bdddb2578c8d88317bd646a37de1dc26b2
iourilitv <iourilitv1965@gmail.com>
2019-10-09 14:20:16 +0300

    Completed the task3 of the lesson 5 hw.

--- src/main/java/lesson5/hw/Less5_hw_3.java
+++ src/main/java/lesson5/hw/Less5_hw_3.java
@@ -6,22 +6,40 @@ package lesson5.hw;
  * Урок 5. Рекурсия
  * Зачем функция вызывает саму себя?
  * Домашнее задание.
- * 3*. Добавить в рекурсивный метод recMultiply возможность перемножения отрицательных чисел.
+ * DONE 3*. Добавить в рекурсивный метод recMultiply возможность перемножения отрицательных чисел.
  */
 public class Less5_hw_3 {
     public static void main(String[] args) {
-        System.out.println(multiply(4, 7));
+        System.out.println("4 * 7 = " + multiply(4, 7));
+        System.out.println("-4 * 7 = " + multiply(-4, 7));
+        System.out.println("4 * -7 = " + multiply(4, -7));
+        System.out.println("-4 * -7 = " + multiply(-4, -7));
     }
 
-    static int multiply(int a, int b) {
-        int value = 0;
-        for (int i = 0; i < b; i++) {
-            value += a;
+    //метод перемножения целочисленных чисел с любым знаком
+    private static int multiply(int a, int b) {
+        //если одно из чисел равно 0, возвращаем 0
+        if (a == 0 || b == 0) {
+            return 0;
         }
-        return value;
+        //если первое число равно 1, возвращаем второе число
+        if (a == 1) {
+            return b;
+        }
+        int neg = 1;
+        //если только одно из чисел отрицательное
+        if((a < 0 && b > 0) || (b < 0 && a > 0)){
+            //берем модули чисел
+            //a = Math.abs(a);
+            //b = Math.abs(b);
+            neg = -1;
+        }
+        return neg * recMultiply(Math.abs(a), Math.abs(b));
     }
 
-    static int recMultiply(int a, int b) {
+    //Рекуперативный метод, где второе число - множитель
+    private static int recMultiply(int a, int b) {
+        //если второе число равно 1, возвращаем первое число
         if (b == 1) {
             return a;
         }
