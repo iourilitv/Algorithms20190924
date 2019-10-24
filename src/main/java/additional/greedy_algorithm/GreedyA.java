67a82c41967079b64e227da7efb108d7517d4ac3
iourilitv <iourilitv1965@gmail.com>
2019-10-24 10:42:20 +0300

    Update the draft of Greedy Algorithm.

--- src/main/java/additional/greedy_algorithm/GreedyA.java
+++ src/main/java/additional/greedy_algorithm/GreedyA.java
@@ -1,4 +1,30 @@
 package additional.greedy_algorithm;
 
-public class GreedyA {
+import java.util.HashMap;
+import java.util.HashSet;
+
+class GreedyA {
+    private final HashMap<String, HashSet<String>> stations;
+    private final HashSet<String> statesNeeded;
+    private HashSet<String> finalStations = new HashSet<>();
+
+    GreedyA(HashMap<String, HashSet<String>> stations, HashSet<String> statesNeeded) {
+        this.stations = stations;
+        this.statesNeeded = statesNeeded;
+        init();
+
+    }
+
+    //TODO временно
+    private void init() {
+        for (HashMap.Entry s: stations.entrySet()) {
+            finalStations.add(s.getKey().toString());
+        }
+
+    }
+
+    //TODO временно
+    HashSet getResult() {
+        return finalStations;
+    }
 }
--- src/main/java/additional/greedy_algorithm/MainGA.java
+++ src/main/java/additional/greedy_algorithm/MainGA.java
@@ -1,39 +1,73 @@
 package additional.greedy_algorithm;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Книга Адитья Бхаргава.Грокаем алгоритмы. Aditya Bhargava.Grokking Algorithms.2017
- * Глава 8. Жадный алгоритм(Greedy algorithm).
+ * Глава 8. Жадные алгоритмы(Greedy algorithms) - это приближенные алгоритмы.
  * Translated by Yuriy Litvinenko from Python to Java.
  * Задача. "О радиостанциях".
+ * Бы открываете собственную авторскую программу на радио и хотите, чтобы вас слушали во всех
+ * 50 штатах. Нужно решить, на каких радиостанциях должна транслироваться ваша передача. Каждая
+ * u станция стоит денег, поэтому количество станции необходимо свести к минимуму.
+ * Имеется список станций со списком штатов покрытия для каждой.
+ * Формализованная задача.
  * Выбрать минимальное количество радиостанций, чтобы покрыть всю территории вещания.
- *
+ * Решение.
+ * 1 . Выбрать станцию, покрывающую наибольшее количество штатов, еще не входящих в покрытие.
+ *     Если станция будет покрывать некоторые штаты, уже входящие в покрытие, это нормально.
+ * 2. Повторять, пока остаются штаты, не входящие в покрытие.
  */
 public class MainGA {
     public static void main(String[] args) {
-        //инициируем взвешенный граф в виде хэш таблицы хэш таблиц весов ребер
-        final HashMap<String, HashMap<String, Integer>> graph = new HashMap<>();
-        //наполняем граф узлами
-        graph.put("book", new HashMap<>());
-        graph.put("poster", new HashMap<>());
-        graph.put("vinyl", new HashMap<>());
-        graph.put("guitar", new HashMap<>());
-        graph.put("drum", new HashMap<>());
-        graph.put("piano", new HashMap<>());
-        //наполняем узлы весами ребер
-        graph.get("book").put("poster", 0);
-        graph.get("book").put("vinyl", 5);
-        graph.get("poster").put("guitar", 30);
-        graph.get("poster").put("drum", 35);
-        graph.get("vinyl").put("guitar", 15);
-        graph.get("vinyl").put("drum", 20);
-        graph.get("guitar").put("piano", 20);
-        graph.get("drum").put("piano", 10);
+        //инициируем список радиостанций в виде хэш таблицы с ключами - их названиями и
+        // хэш таблиц с их покрытиями вещания
+        final HashMap<String, HashSet<String>> stations = new HashMap<>();
+        //инициируем список требуемых штатов покрытия вещанием в виде хэш набора
+        // с ключами - их названиями
+        //"mt" , "wa" , "or", "id", "nv", "ut", "са", "az"
+        final HashSet<String> statesNeeded = new HashSet<>();
 
-        GreedyA greedyA = new GreedyA();
-        //System.out.println(greedyA.toString());
+        //наполняем список станций
+        stations.put("kone", new HashSet<>());
+        stations.put("ktwo", new HashSet<>());
+        stations.put("kthree", new HashSet<>());
+        stations.put("kfour", new HashSet<>());
+        stations.put("kfive", new HashSet<>());
+        //наполняем покрытиями список станций
+        stations.get("kone").add("id");
+        stations.get("kone").add("nv");
+        stations.get("kone").add("ut");
+        stations.get("ktwo").add("wa");
+        stations.get("ktwo").add("id");
+        stations.get("ktwo").add("mt");
+        stations.get("kthree").add("or");
+        stations.get("kthree").add("nv");
+        stations.get("kthree").add("ca");
+        stations.get("kfour").add("nv");
+        stations.get("kfour").add("ut");
+        stations.get("kfive").add("ca");
+        stations.get("kfive").add("az");
+
+
+        GreedyA greedyA = new GreedyA(stations, statesNeeded);
+        System.out.println(greedyA.getResult());
 
 
     }
 }
+
+//        stations [ " kone " ] = set ( [ " id " , " nv " , " ut " ] )
+//        stations [ " ktwo " ] = set ( [ " wa " , " id " , " mt " ] )
+//        stations [ " kthree " ] = set ( [ " or " , " nv " , " са " ] )
+//        stations [ " kfour " ] = set ( [ " nv " , " ut " ] )
+//        stations [ " kfive " ] = set ( [ " ca " , " az " ] )
+
+
+//        System.out.println(stations);
+//        {kfour=[nv, ut], ktwo=[mt, wa, id], kone=[nv, id, ut], kthree=[or, nv, ca], kfive=[az, ca]}
+//        stations.put("kone", new HashSet<>());
+//        stations.get("kfour").add("ut");
+//        System.out.println(stations);
+//        {kfour=[nv, ut], ktwo=[mt, wa, id], kone=[], kthree=[or, nv, ca], kfive=[az, ca]}
